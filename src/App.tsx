1c3145c0485b444fd49e472637bd67fe6b303726
m00nsh <mo3ov.877@gmail.com>
2024-07-26 13:02:05 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import "./post";
 
 function App() {
   return (
-    <div>
-      <h1>課題1</h1>
-    </div>
+    <>
+      <header className="bg-slate-700 text-white w">
+        <div className="container flex mx-auto p-5 text-xl">
+          <a href="#home" className="font-medium ml-3">
+            ブログ
+          </a>
+          <a href="#contact" className="font-medium ml-auto text-base">
+            お問い合わせ
+          </a>
+        </div>
+      </header>
+      <section>{/* list */}</section>
+      <div>{/* contact */}</div>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/App.{css,tsx}",
+    "./src/main.tsx",
+    "./src/post.js",
+    "./src/constance.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
