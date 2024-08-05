7f0566dd33e85bf1e4b00fcb23f03eb24c3da944
misaki <154414725+m00nsh@users.noreply.github.com>
2024-08-05 10:36:22 +0900

    Update Post.jsx
    hennkou

--- src/Post.jsx
+++ src/Post.jsx
@@ -10,18 +10,15 @@ export const Post = (props) => {
           <div className="flex">
             {
               // post.categoriesが存在し、かつ配列である場合
-              posts.categories && Array.isArray(posts.categories){ // カテゴリーをループして表示
-                  posts.categories.map((category, i) => (
-                    <p
-                      key={i}
-                      className="border border-blue-500 rounded text-blue-500 p-1"
-                    >
-                      {category}
-                    </p>
-                  ))
-                } // カテゴリーが存在しない場合は何も表示しない
-                  null
-            }
+              posts.categories && Array.isArray(posts.categories) // カテゴリーをループして表示
+                    {post.categories.map((category, i) => (
+    <p
+      key={i}
+      className="border border-blue-500 rounded text-blue-500 p-1"
+    >
+      {category}
+    </p>
+  ))}
           </div>
         </div>
 <div dangerouslySetInnerHTML={{ __html: post.content }} className="line-clamp-2" /></div>
