af3dce4d7e476e0a565179f10ba3aeca8658f7de
m00nsh <mo3ov.877@gmail.com>
2024-09-19 13:33:21 +0900

    課題4の実装

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Posts } from "./Posts";
 import { Header } from "./page/Header";
 
-import { Article } from "./Article";
-import Article from "./Article";
+import { Form } from "./page/contact/Form";
 
+import { Article } from "./Article";
 
 export const App = () => {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Form />} />
         <Route path="/" element={<Posts />} />
         <Route path="/article/:id" element={<Article />} />
       </Routes>
